448e3d7dbcf423c8b429f22263b2711440ada7cf
Fábio Salata <s2it_fsalata@uolinc.com>
2020-09-07 15:47:19 -0300

    Creating project
    adding resources

---
+++ iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Fábio Salata on 07/09/20.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
