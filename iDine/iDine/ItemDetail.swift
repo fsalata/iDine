e9d4ecd944d949794ddc86da3fc039f6d0e04677
Fábio Salata <s2it_fsalata@uolinc.com>
2020-09-07 16:33:43 -0300

    Menu list and detail view

---
+++ iDine/iDine/ItemDetail.swift
@@ -0,0 +1,41 @@
+//
+//  ItemDetail.swift
+//  iDine
+//
+//  Created by Fábio Salata on 07/09/20.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    var item: MenuItem
+    
+    var body: some View {
+        VStack {
+            ZStack (alignment: .bottomTrailing) {
+                Image(item.mainImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("Photo: \(item.photoCredit)")
+                    .padding(4)
+                    .background(Color.black.opacity(0.6))
+                    .font(.caption)
+                    .foregroundColor(.white)
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(item.description)
+                .padding()
+            
+            Spacer()
+        }.navigationBarTitle(Text(item.name), displayMode: .inline)
+    }
+}
+
+struct ItemDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            ItemDetail(item: MenuItem.example)
+        }
+    }
+}
